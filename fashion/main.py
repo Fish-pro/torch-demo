028b3718cbd03abd3f572dad10864ec9b9b5d6fe
Zechun Chen <zechun.chen@daocloud.io>
2024-05-27 11:06:00 +0800

    add load mode to test

--- fashion/main.py
+++ fashion/main.py
@@ -108,3 +108,27 @@ print("Done!")
 
 torch.save(model.state_dict(), "model.pth")
 print("Saved PyTorch Model State to model.pth")
+
+new_model = NeuralNetwork().to(device)
+new_model.load_state_dict(torch.load("model.pth"))
+
+classes = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
+
+new_model.eval()
+x, y = test_data[0][0], test_data[0][1]
+with torch.no_grad():
+    x = x.to(device)
+    pred = model(x)
+    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+    print(f'Predicted:"{predicted}", Actual: "{actual}"')
