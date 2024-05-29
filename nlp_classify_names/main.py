451413f8dbc092dd1b9a7a4785456b80c4489c4b
Zechun Chen <zechun.chen@daocloud.io>
2024-05-29 17:21:23 +0800

    add nil example

---
+++ nlp_classify_names/main.py
@@ -0,0 +1,108 @@
+from io import open
+import glob
+import os
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def findFiles(path): return glob.glob(path)
+
+
+# print(findFiles('data/names/*.txt'))
+
+import unicodedata
+import string
+
+all_letters = string.ascii_letters + " .,;'"
+n_letters = len(all_letters)
+
+
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+        and c in all_letters
+    )
+
+
+# print(unicodeToAscii('Ślusàrski'))
+
+category_lines = {}
+all_categories = []
+
+
+def readLines(filename):
+    lines = open(filename, encoding='utf-8').read().strip().split('\n')
+    return [unicodeToAscii(line) for line in lines]
+
+
+for filename in findFiles('data/names/*.txt'):
+    category = os.path.splitext(os.path.basename(filename))[0]
+    all_categories.append(category)
+    lines = readLines(filename)
+    category_lines[category] = lines
+
+n_categories = len(all_categories)
+
+
+# print(category_lines['Italian'][:5])
+
+def letterToindex(letter):
+    return all_letters.find(letter)
+
+
+def letterToTensor(letter):
+    tensor = torch.zeros(1, n_letters)
+    tensor[0][letterToindex(letter)] = 1
+    return tensor
+
+
+def lineToTensor(line):
+    tensor = torch.zeros(len(line), 1, n_letters)
+    for li, letter in enumerate(line):
+        tensor[li][0][letterToindex(letter)] = 1
+    return tensor
+
+
+print(letterToTensor('J'))
+print(lineToTensor('Jones').size())
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.i2h = nn.Linear(input_size, hidden_size)
+        self.h2h = nn.Linear(hidden_size, hidden_size)
+        self.h2o = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        hidden = F.tanh(self.i2h(input) + self.h2h(hidden))
+        output = self.h2o(hidden)
+        output = self.softmax(output)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
+
+
+n_hidden = 128
+rnn = RNN(n_letters, n_hidden, n_categories)
+
+input = letterToTensor('Albert')
+hidden = torch.zeros(1, n_hidden)
+
+output, next_hidden = rnn(input[0], hidden)
+print(output)
+
+
+def categoryFromOutput(output):
+    top_n, top_i = output.topk(1)
+    category_i = top_i[0].item()
+    return all_categories[category_i], category_i
+
+
+print(categoryFromOutput(output))
