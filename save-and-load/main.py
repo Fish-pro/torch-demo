1690a0d0919194cf3048a0ad2f0799643ba445c4
Zechun Chen <zechun.chen@daocloud.io>
2024-05-29 10:03:44 +0800

    add load examples

--- save-and-load/main.py
+++ save-and-load/main.py
@@ -2,4 +2,8 @@ import torch
 import torchvision.models as models
 
 model = models.vgg16("IMAGENET1K_V1")
-torch.save(model.state_dict(), "model_weights.pth")
+torch.save(model.state_dict(), 'model_weights.pth')
+
+model = models.vgg16()
+model.load_state_dict(torch.load('model_weights.pth'))
+print(model.eval())
