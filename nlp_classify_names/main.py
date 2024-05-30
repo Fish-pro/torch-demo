d6d3ee405cc856ef008eca1383067924d88fe357
Zechun Chen <zechun.chen@daocloud.io>
2024-05-30 14:43:06 +0800

    add names example

--- nlp_classify_names/main.py
+++ nlp_classify_names/main.py
@@ -1,23 +1,21 @@
 from io import open
 import glob
 import os
-
+import unicodedata
+import string
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+import random
+import time
+import math
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 def findFiles(path): return glob.glob(path)
 
-
-# print(findFiles('data/names/*.txt'))
-
-import unicodedata
-import string
-
 all_letters = string.ascii_letters + " .,;'"
 n_letters = len(all_letters)
-
 
 def unicodeToAscii(s):
     return ''.join(
@@ -26,17 +24,12 @@ def unicodeToAscii(s):
         and c in all_letters
     )
 
-
-# print(unicodeToAscii('Ślusàrski'))
-
 category_lines = {}
 all_categories = []
-
 
 def readLines(filename):
     lines = open(filename, encoding='utf-8').read().strip().split('\n')
     return [unicodeToAscii(line) for line in lines]
-
 
 for filename in findFiles('data/names/*.txt'):
     category = os.path.splitext(os.path.basename(filename))[0]
@@ -46,29 +39,22 @@ for filename in findFiles('data/names/*.txt'):
 
 n_categories = len(all_categories)
 
-
-# print(category_lines['Italian'][:5])
-
-def letterToindex(letter):
+def letterToIndex(letter):
     return all_letters.find(letter)
-
 
 def letterToTensor(letter):
     tensor = torch.zeros(1, n_letters)
-    tensor[0][letterToindex(letter)] = 1
+    tensor[0][letterToIndex(letter)] = 1
     return tensor
-
 
 def lineToTensor(line):
     tensor = torch.zeros(len(line), 1, n_letters)
     for li, letter in enumerate(line):
-        tensor[li][0][letterToindex(letter)] = 1
+        tensor[li][0][letterToIndex(letter)] = 1
     return tensor
-
 
 print(letterToTensor('J'))
 print(lineToTensor('Jones').size())
-
 
 class RNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
@@ -88,7 +74,6 @@ class RNN(nn.Module):
     def initHidden(self):
         return torch.zeros(1, self.hidden_size)
 
-
 n_hidden = 128
 rnn = RNN(n_letters, n_hidden, n_categories)
 
@@ -98,11 +83,121 @@ hidden = torch.zeros(1, n_hidden)
 output, next_hidden = rnn(input[0], hidden)
 print(output)
 
-
 def categoryFromOutput(output):
     top_n, top_i = output.topk(1)
     category_i = top_i[0].item()
     return all_categories[category_i], category_i
 
-
 print(categoryFromOutput(output))
+
+def randomChoice(l):
+    return l[random.randint(0, len(l) - 1)]
+
+def randomTrainingExample():
+    category = randomChoice(all_categories)
+    line = randomChoice(category_lines[category])
+    category_tensor = torch.tensor([all_categories.index(category)], dtype=torch.long)
+    line_tensor = lineToTensor(line)
+    return category, line, category_tensor, line_tensor
+
+for i in range(10):
+    category, line, category_tensor, line_tensor = randomTrainingExample()
+    print('category =', category, '/ line =', line)
+
+criterion = nn.NLLLoss()
+learning_rate = 0.005
+
+def train(category_tensor, line_tensor):
+    hidden = rnn.initHidden()
+    rnn.zero_grad()
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+    loss = criterion(output, category_tensor)
+    loss.backward()
+    for p in rnn.parameters():
+        p.data.add_(p.grad.data, alpha=-learning_rate)
+    return output, loss.item()
+
+n_iters = 100000
+print_every = 5000
+plot_every = 1000
+
+current_loss = 0
+all_losses = []
+
+def timeSince(since):
+    now = time.time()
+    s = now - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+start = time.time()
+
+for iter in range(1, n_iters + 1):
+    category, line, category_tensor, line_tensor = randomTrainingExample()
+    output, loss = train(category_tensor, line_tensor)
+    current_loss += loss
+    if iter % print_every == 0:
+        guess, guess_i = categoryFromOutput(output)
+        correct = '✓' if guess == category else '✗ (%s)' % category
+        print('%d %d%% (%s) %.4f %s / %s %s' % (iter, iter / n_iters * 100, timeSince(start), loss, line, guess, correct))
+    if iter % plot_every == 0:
+        all_losses.append(current_loss / plot_every)
+        current_loss = 0
+
+plt.figure()
+plt.plot(all_losses)
+plt.show()
+
+confusion = torch.zeros(n_categories, n_categories)
+n_confusion = 10000
+
+def evaluate(line_tensor):
+    hidden = rnn.initHidden()
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+    return output
+
+for i in range(n_confusion):
+    category, line, category_tensor, line_tensor = randomTrainingExample()
+    output = evaluate(line_tensor)
+    guess, guess_i = categoryFromOutput(output)
+    category_i = all_categories.index(category)
+    confusion[category_i][guess_i] += 1
+
+for i in range(n_categories):
+    confusion[i] = confusion[i] / confusion[i].sum()
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+cax = ax.matshow(confusion.numpy())
+fig.colorbar(cax)
+
+ax.set_xticklabels([''] + all_categories, rotation=90)
+ax.set_yticklabels([''] + all_categories)
+
+ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+plt.show()
+
+
+def predict(input_line, n_predictions=3):
+    print('\n> %s' % input_line)
+    with torch.no_grad():
+        output = evaluate(lineToTensor(input_line))
+
+        # Get top N categories
+        topv, topi = output.topk(n_predictions, 1, True)
+        predictions = []
+
+        for i in range(n_predictions):
+            value = topv[0][i].item()
+            category_index = topi[0][i].item()
+            print('(%.2f) %s' % (value, all_categories[category_index]))
+            predictions.append([value, all_categories[category_index]])
+
+predict('Dovesky')
+predict('Jackson')
+predict('Satoshi')
